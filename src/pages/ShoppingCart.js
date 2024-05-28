15564439bb59d9ccfee05652c5562b3983b588ba
DanielSaborido <dsaborio@iesrafaelalberti.es>
2024-05-28 21:30:12 +0200

    base para las compras

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,34 +1,139 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Button, Modal, Form, Radio, message } from 'antd'
 
 const ShoppingCart = () => {
   const token = localStorage.getItem('token')
   const customer = token.split('_')[1]
-  const [carts, setCarts] = useState([])
+  const [cart, setCart] = useState({})
   const [lastCart, setLastCart] = useState([])
   const [totalCost, setTotalCost] = useState(0)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [paymentMethodVisible, setPaymentMethodVisible] = useState(false)
+  const [form] = Form.useForm()
+  const [paymentMethod, setPaymentMethod] = useState({
+    cardNumber: "",
+    expiryDate: "",
+    cvv: ""})
 
   useEffect(() => {
-    axios.get(`/orders/${customer}`)
-      .then(response => {
-        setCarts(response.data)
+    axios.get(`/orders/${customer}/actual`)
+     .then(response => {
+        setCart(response.data)
       })
-      .catch(error => {
+     .catch(error => {
         console.error('There was an error!', error)
       })
   }, [customer])
 
   useEffect(() => {
-    if (carts.length !== 0){
-      axios.get(`/order-details/${carts[carts.length-1].id}`)
+    if (cart!= {}) {
+      axios.get(`/order-details/${cart.id}`)
+       .then(response => {
+          setLastCart(response.data)
+        })
+       .catch(error => {
+          console.error('There was an error!', error)
+        })
+    }
+  }, [cart])
+
+  const handleOk = () => {
+    form.validateFields().then(values => {
+      let paymentMethod = values.paymentMethod
+      let status = ''
+  
+      if (paymentMethod === 'creditCard' || paymentMethod === 'debitCard') {
+        if (handlePaymentMethod(customer)) {
+          status = 'paid'
+        } else {
+          setPaymentMethodVisible(true)
+          return
+        }
+      } else if (paymentMethod === 'cash') {
+        status = 'cash'
+      } else if (paymentMethod === 'store') {
+        status = 'store'
+      }
+  
+      axios.put(`/orders/${cart.id}`, {...cart, status: status})
+     .then(response => {
+        console.log(response.data)
+        setIsModalVisible(false)
+      })
+     .catch(error => {
+        console.error('There was an error!', error)
+      })
+    })
+  }
+
+  const handlePaymentMethod = (customer) => {
+    axios.get(`/payment-methods/${customer}`)
+     .then(response => {
+        if (response.data.success){
+          message.success('Pago realizado exitosamente')
+          return response.data.success
+        }
+        return response.data.success
+      })
+     .catch(error => {
+        console.error('There was an error!', error)
+      })
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+
+  const showPaymentModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleAddPaymentMethod = (event) => {
+    event.preventDefault()
+    const { cardNumber, expiryDate, cvv} = paymentMethod
+    if (!cardNumber ||!expiryDate ||!cvv) {
+      message.error("Por favor, complete todos los campos")
+      return
+    }
+
+    const cardNumberRegex = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11})$/
+    if (!cardNumberRegex.test(cardNumber)) {
+      message.error("Número de tarjeta no válido")
+      setPaymentMethod({ ...paymentMethod, cardNumber: null })
+      return
+    }
+  
+    const expiryDateRegex = /^(0[1-9]|1[0-2])\/[0-9]{2}$/
+    if (!expiryDateRegex.test(expiryDate)) {
+      message.error("Fecha de vencimiento no válida (MM/YY)")
+      setPaymentMethod({ ...paymentMethod, expiryDate: null })
+      return
+    }
+  
+    const cvvRegex = /^[0-9]{3,4}$/
+    if (!cvvRegex.test(cvv)) {
+      message.error("CVV no válido")
+      setPaymentMethod({ ...paymentMethod, cvv: null })
+      return
+    }
+    axios.post('/payment-methods', {...paymentMethod,customer_id:customer})
+    .then(response => {
+      message.success('tarjeta agregada')
+      axios.put(`/orders/${cart.id}`, {...cart, status: 'paid'})
       .then(response => {
-        setLastCart(response.data)
+        console.log(response.data)
+        setIsModalVisible(false)
       })
       .catch(error => {
         console.error('There was an error!', error)
       })
-    }
-  }, [carts])
+    })
+    .catch(error => {
+      console.error('There was an error!', error)
+    })
+    setPaymentMethodVisible(false)
+  }
 
   return (
     <div>
@@ -40,21 +145,52 @@ const ShoppingCart = () => {
           <tr>Precio</tr>
         </thead>
         <tbody>
-            {lastCart.map((product) => {
-              setTotalCost(totalCost+=(product.total_cost))
-              return (
-                <tr key={product.id}>
-                  <td>{product.product_name}</td>
-                  <td>{product.quantity}</td>
-                  <td>{product.total_cost}</td>
-                </tr>
-              )
-            })}
+          {lastCart.map((product) => {
+            setTotalCost(totalCost += product.total_cost)
+            return (
+              <tr key={product.id}>
+                <td>{product.product_name}</td>
+                <td>{product.quantity}</td>
+                <td>{product.total_cost}</td>
+              </tr>
+            )
+          })}
           <tr>
             <td colSpan={3}>Coste total: {totalCost}€</td>
           </tr>
         </tbody>
       </table>
+      <Button type="primary" onClick={showPaymentModal}>
+        Finalizar compra
+      </Button>
+      <Modal
+        title="Seleccionar método de pago"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form form={form} layout="vertical">
+          <Form.Item name="paymentMethod" rules={[{ required: true, message: 'Por favor, seleccione un método de pago.' }]}>
+            <Radio.Group>
+              <Radio value="creditCard">Tarjeta de crédito</Radio>
+              <Radio value="debitCard">Tarjeta de débito</Radio>
+              <Radio value="cash">Efectivo</Radio>
+              <Radio value="store">En tienda</Radio>
+            </Radio.Group>
+          </Form.Item>
+        </Form>
+      </Modal>
+      {paymentMethodVisible&&
+        <div>
+          <h1>Agregar método de pago</h1>
+          <form onSubmit={handleAddPaymentMethod}>
+            <input type="text" placeholder="Número de tarjeta" onChange={e => setPaymentMethod({ ...paymentMethod, cardNumber: e.target.value })} required />
+            <input type="text" placeholder="Fecha de vencimiento" onChange={e => setPaymentMethod({ ...paymentMethod, expiryDate: e.target.value })} required />
+            <input type="text" placeholder="CVV" onChange={e => setPaymentMethod({ ...paymentMethod, cvv: e.target.value })} required />
+            <button type="submit">Agregar método de pago</button>
+          </form>
+        </div>
+      }
     </div>
   )
 }
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Button, Modal, Form, Radio, message } from 'antd'
 
 const User = ({userId}) => {
   const [user, setUser] = useState({})
@@ -8,6 +9,11 @@ const User = ({userId}) => {
   const [orderDetails, setOrderDetails] = useState([])
   const [orderId, setOrderId] = useState(null)
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const [paymentMethodVisible, setPaymentMethodVisible] = useState(false)
+  const [paymentMethod, setPaymentMethod] = useState({
+    cardNumber: "",
+    expiryDate: "",
+    cvv: ""})
 
   useEffect(() => {
     if (userId) {
@@ -52,12 +58,51 @@ const User = ({userId}) => {
     })
   }
 
+  const handleAddPaymentMethod = (event) => {
+    event.preventDefault()
+    const { cardNumber, expiryDate, cvv} = paymentMethod
+    if (!cardNumber ||!expiryDate ||!cvv) {
+      message.error("Por favor, complete todos los campos")
+      return
+    }
+
+    const cardNumberRegex = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11})$/
+    if (!cardNumberRegex.test(cardNumber)) {
+      message.error("Número de tarjeta no válido")
+      setPaymentMethod({ ...paymentMethod, cardNumber: null })
+      return
+    }
+  
+    const expiryDateRegex = /^(0[1-9]|1[0-2])\/[0-9]{2}$/
+    if (!expiryDateRegex.test(expiryDate)) {
+      message.error("Fecha de vencimiento no válida (MM/YY)")
+      setPaymentMethod({ ...paymentMethod, expiryDate: null })
+      return
+    }
+  
+    const cvvRegex = /^[0-9]{3,4}$/
+    if (!cvvRegex.test(cvv)) {
+      message.error("CVV no válido")
+      setPaymentMethod({ ...paymentMethod, cvv: null })
+      return
+    }
+    axios.post('/payment-methods', {...paymentMethod,customer_id:userId})
+    .then(response => {
+      message.success('tarjeta agregada')
+    })
+    .catch(error => {
+      console.error('There was an error!', error)
+    })
+    setPaymentMethodVisible(false)
+  }
+
   return (
     <>
       <section>
         <img src={user.picture}/>
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <button onClick={setPaymentMethodVisible(true)}>Agregar metodo de pago</button>
       </section>
       <section>
         <ul>
@@ -66,6 +111,17 @@ const User = ({userId}) => {
           })}
         </ul>
       </section>
+      {paymentMethodVisible&&
+        <div>
+          <h1>Agregar método de pago</h1>
+          <form onSubmit={handleAddPaymentMethod}>
+            <input type="text" placeholder="Número de tarjeta" onChange={e => setPaymentMethod({ ...paymentMethod, cardNumber: e.target.value })} required />
+            <input type="text" placeholder="Fecha de vencimiento" onChange={e => setPaymentMethod({ ...paymentMethod, expiryDate: e.target.value })} required />
+            <input type="text" placeholder="CVV" onChange={e => setPaymentMethod({ ...paymentMethod, cvv: e.target.value })} required />
+            <button type="submit">Agregar método de pago</button>
+          </form>
+        </div>
+      }
       {isModalVisible && (
         <Modal title={'Detalles de la orden'} visible={isModalVisible} onCancel={() => {setIsModalVisible(false);setTotalCost(0)}}>
           <table>
